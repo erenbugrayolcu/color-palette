43e3cb569168aea6acf32a761dad4e2301fc7a7b
erenbugrayolcu <63672367+erenbugrayolcu@users.noreply.github.com>
2021-04-23 01:38:49 +0300

    Update main.js

--- main.js
+++ main.js
@@ -48,6 +48,7 @@ function generateColorPalette(){
         li.addEventListener('click', (e) => {
             let targetInput = currentElem.querySelector('input[name="color"]');
             readonly="readonly";
+            $('#input_92_31').attr('readonly','readonly');
             targetInput.select();
             document.execCommand('copy');
             notification('Color <b>' + targetInput.value  + '</b> copied to your clipboard');
